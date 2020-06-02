e5b392b1e020a476c442c96c2ebf7d738ec1494b
Sean-Stilwell <sstil051@uottawa.ca>
2020-06-02 14:59:18 -0400

    Adding the second release

--- app/src/main/java/app/game/lightsout/MainActivity.java
+++ app/src/main/java/app/game/lightsout/MainActivity.java
@@ -3,6 +3,7 @@ package app.game.lightsout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -30,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
             case R.id.button3:
                 Intent intent3 = new Intent(MainActivity.this, HardGame.class);
                 startActivity(intent3);
+                break;
+            case R.id.button5:
+                // TODO Auto-generated method stub
+                final String myappPackageName = getPackageName(); // getPackageName() from Context or Activity object
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + myappPackageName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + myappPackageName)));
+                }
                 break;
         }
     }
